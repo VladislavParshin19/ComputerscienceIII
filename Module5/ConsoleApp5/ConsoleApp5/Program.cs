6562eb3bd8a802e7807109d5529ad4d870b63ee3
Vladislav Parshin <vludparshin@gmail.com>
2023-10-14 19:32:38 +0600

    Добавьте файлы проекта.

---
+++ Module5/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+//Реализовать несколько методов или классов с методами и вызвать один метод из другого. В вызываемом методе сгенерировать исключение и «поднять» его в вызывающий метод.
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                // Запрашиваем делимое и делитель у пользователя
+                Console.Write("Введите делимое: ");
+                int dividend = int.Parse(Console.ReadLine());
+
+                Console.Write("Введите делитель: ");
+                int divisor = int.Parse(Console.ReadLine());
+
+                // Выполняем деление и обрабатываем исключения
+                PerformDivision(dividend, divisor);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Поймано исключение: " + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void PerformDivision(int dividend, int divisor)
+        {
+            if (divisor == 0)
+            {
+                // Генерируем исключение, если делитель равен нулю
+                throw new DivideByZeroException("Деление на ноль недопустимо");
+            }
+            // Выполняем деление и выводим результат
+            int result = dividend / divisor;
+            Console.WriteLine("Результат деления: " + result);
+        }
+    }
+}
---
+++ Module5/ConsoleApp5D1/ConsoleApp5D1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+//Перехватить исключение запроса к несуществующему веб ресурсу и вывести сообщение о том, что произошла ошибка. Программа должна завершиться без ошибок.
+
+namespace ConsoleApp5D1
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                string url = "https://https://sigmamod.com";
+                var request = System.Net.WebRequest.Create(url);
+                var response = request.GetResponse();
+                // Дополнительный код обработки успешного ответа, если необходимо
+                response.Close();
+            }
+            catch (System.Net.WebException ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
