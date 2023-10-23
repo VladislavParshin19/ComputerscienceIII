b031f0757ffff34841919f2c90027952fa222e8e
Vladislav Parshin <vludparshin@gmail.com>
2023-10-23 21:50:50 +0600

    Module7

---
+++ Module7/Day7 (Tanks)/Tank.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Day7__Tanks_
+{
+    public class Tank
+    {
+        private string tankName;
+        private int ammoLvl;
+        private int armorLvl;
+        private int speedLvl;
+
+        // Конструктор для инициализации параметров танка
+        public Tank(string name, Random random)
+        {
+            tankName = name;
+            random = random;
+            ammoLvl = random.Next(0, 100);
+            armorLvl = random.Next(0, 100);
+            speedLvl = random.Next(0, 100);
+        }
+
+        // Метод для получения параметров танка в виде строки
+        public string GetTankParameters()
+        {
+            return $"{tankName}: Ammo - {ammoLvl}%, Armor - {armorLvl}%, Maneuverability - {speedLvl}%";
+        }
+        // Перегрузка оператора '*' (произведение) для определения победы в бою
+        public static bool operator *(Tank tank1, Tank tank2)
+        {
+            int winCriteria = 0;
+
+            // Проверка параметров танков и определение победителя
+            if (tank1.ammoLvl > tank2.ammoLvl)
+            {
+                winCriteria++;
+            }
+            if (tank1.armorLvl > tank2.armorLvl)
+            {
+                winCriteria++;
+            }
+            if (tank1.speedLvl > tank2.speedLvl)
+            {
+                winCriteria++;
+            }
+
+            //Победитель должен превосходить проигравшего по не менее чем двум параметрам
+            return winCriteria >= 2;
+
+        }
+    }
+}
---
+++ Module7/Module7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Day7__Tanks_;
+
+namespace Module7
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Random random = new Random();
+                Tank t34 = new Tank("T-34", random);
+                Tank pantera = new Tank("Pantera", random);
+
+                Console.WriteLine($"Сражение {i + 1}:");
+                Console.WriteLine(t34.GetTankParameters());
+                Console.WriteLine(pantera.GetTankParameters());
+
+                if (t34 * pantera)
+                {
+                    Console.WriteLine("Победил Т-34");
+                }
+                else
+                {
+                    Console.WriteLine("Победил Pantera");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
